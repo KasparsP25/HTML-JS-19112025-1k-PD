747f0dbf19abde000309b6310ae60379a9396820
Edgars G <Edgars@gmail>
2025-11-19 15:47:27 +0200

    added ĶMI message

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ const masInput = document.getElementById("masInput");
 const heInput = document.getElementById("heInput");
 const output = document.getElementById("output");
 const button = document.getElementById("button");
+//const history = document.getElementById("history");
 
 button.addEventListener(`click`, massCalc)
 
 function massCalc(){
-    let outputText = masInput.value / (heInput.value * heInput.value)
-    console.log ("Jūsu ĶMI ir " + outputText)
+    let outputNum = masInput.value / (heInput.value * heInput.value)
+    if (outputNum <= 19){
+        outputR = (" (Nepietiekama ķermeņa masa)")
+    } else 
+    if (outputNum <= 25){
+        outputR = (" (Normāla ķermeņa masa)")
+    } else 
+    if (outputNum <= 30){
+        outputR = (" (Lieka ķermeņa masa)")
+    } else 
+    outputR = (" (Aptaukošanās)") 
+
+    console.log("Jūsu ĶMI ir " + outputNum + outputR)
 }
+
