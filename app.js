f82aedcc90fc0e68f9c622e39505a86e04c040bf
Edgars G <Edgars@gmail>
2025-11-19 16:16:26 +0200

    `finished`

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const masInput = document.getElementById("masInput");
 const heInput = document.getElementById("heInput");
-const output = document.getElementById("output");
+const output = document.getElementById("outputt");
 const button = document.getElementById("button");
-//const history = document.getElementById("history");
+// const history = document.getElementById("history");
 
 button.addEventListener(`click`, massCalc)
 
 function massCalc(){
-    let outputNum = masInput.value / (heInput.value * heInput.value)
+    let heiInput = heInput.value * heInput.value
+    let outputNum = masInput.value / heiInput
     if (outputNum <= 19){
         outputR = (" (Nepietiekama ķermeņa masa)")
     } else 
-    if (outputNum <= 25){
-        outputR = (" (Normāla ķermeņa masa)")
-    } else 
-    if (outputNum <= 30){
-        outputR = (" (Lieka ķermeņa masa)")
-    } else 
-    outputR = (" (Aptaukošanās)") 
+        if (outputNum <= 25){
+            outputR = (" (Normāla ķermeņa masa)")
+        } else 
+            if (outputNum <= 30){
+                outputR = (" (Lieka ķermeņa masa)")
+            } else 
+                outputR = (" (Aptaukošanās)") 
+                
+                console.log("Jūsu ĶMI ir " + outputNum + outputR)
+            }
 
-    console.log("Jūsu ĶMI ir " + outputNum + outputR)
-}
 
+//  historyItem = document.createElement('li')
+//  historyItem.innerHTML = `${masInput.value} + "KG" + " + " + ${heInput.value} + "cm"`;
+//  history.appendChild(historyItem)
