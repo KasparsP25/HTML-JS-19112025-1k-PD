8a4063df70865cb7490b257ff08e3526b3b1896f
EmilsPT2022 <emils@gmail.com>
2025-12-01 17:27:49 +0200

    fully finished

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-const masInput = document.getElementById("masInput");
+const massInput = document.getElementById("massInput");
 const heInput = document.getElementById("heInput");
-const output = document.getElementById("outputt");
+const output = document.getElementById("output");
 const button = document.getElementById("button");
-// const history = document.getElementById("history");
+const history = document.createDocumentFragment("history");
 
 button.addEventListener(`click`, massCalc)
 
 function massCalc(){
-    let heiInput = heInput.value * heInput.value
-    let outputNum = masInput.value / heiInput
+    let heightMeter = heInput.value * 0.01
+    //cm -> m
+    let outputNum = massInput.value / (heightMeter * heightMeter)
+    // mass (kg) / height (m^2)
+    //console.log (outputNum)
     if (outputNum <= 19){
-        outputR = (" (Nepietiekama ķermeņa masa)")
-    } else 
-        if (outputNum <= 25){
-            outputR = (" (Normāla ķermeņa masa)")
-        } else 
-            if (outputNum <= 30){
-                outputR = (" (Lieka ķermeņa masa)")
-            } else 
-                outputR = (" (Aptaukošanās)") 
-                
-                console.log("Jūsu ĶMI ir " + outputNum + outputR)
-            }
+        outputR = " (Nepietiekama ķermeņa masa)"
+    } else if (outputNum <= 25){
+        outputR = " (Normāla ķermeņa masa)"
+    } else if (outputNum <= 30){
+        outputR = " (Lieka ķermeņa masa)"
+    } else {
+        outputR = " (Aptaukošanās)"
+    }
+    output.innerHTML = ("Jūsu ĶMI ir " + outputNum + outputR)
 
 
-//  historyItem = document.createElement('li')
-//  historyItem.innerHTML = `${masInput.value} + "KG" + " + " + ${heInput.value} + "cm"`;
-//  history.appendChild(historyItem)
+    const historyItem = history.appendChild(document.createElement(`li`));
+    historyItem.innerHTML = `${massInput.value}KG / ${heInput.value}cm = ${outputNum + outputR}`;
+    document.body.appendChild(history);
+}
+    // historyItem = document.createElement('li')
+    // historyItem.innerHTML = `${masInput.value}KG + ${heInput.value}cm = ${output}`
+    // history.appendChild(historyItem);
+
+
